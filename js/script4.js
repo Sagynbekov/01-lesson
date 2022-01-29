f80f2e62a012c43662281a48dbbcffc15ba57709
Sagynbekov Adilet <adilet.sagynbekov.kk@gmail.com>
2022-01-29 17:38:15 +0600

    fixed bugs

--- js/script4.js
+++ js/script4.js
@@ -1,8 +1,11 @@
 //Задание №4
+let souvenirWeight = 75;
+let baubleWeight = 112;
+
 let souvenir = prompt("Сколько сувениров вы купили?");
 let bauble = prompt("Сколько игрушек вы купили?");
 
-let result = souvenir * bauble;
-alert("Общий вес посылки: " + result);
+let result = (souvenirWeight * souvenir) + (baubleWeight * bauble);
+alert("Общий вес посылки: " + result + " г");
 
-console.log("Общий вес посылки составляет: " + result);
+console.log("Общий вес посылки составляет: " + result + " г");
